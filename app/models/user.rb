ce42f0075060d31f071088b4b2a297b33f174640
Randy Schmidt <randy@forge38.com>
2012-12-09 14:33:50 -0500

    User#from_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
   has_many :emails, dependent: :destroy
 
-  def self.from_email(email)
-
+  def self.from_email(address)
+    if email = Email.find_by_address(address)
+      email.user
+    else
+      user = create
+      user.emails.create(address: address)
+      user
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,48 @@ describe User do
   context "associations" do
     it { should have_many(:emails).dependent(:destroy) }
   end
+
+  describe ".from_email(email)" do
+    context "when the email doesn't exist" do
+      it "should create a new user" do
+        expect {
+          User.from_email('bob@bob.com')
+        }.to change(User, :count).by(1)
+      end
+
+      it "should create a new email" do
+        expect {
+          @user = User.from_email('bob@bob.com')
+          email = @user.emails.first
+        }.to change(Email, :count).by(1)
+
+        email = Email.find_by_address('bob@bob.com')
+        email.user.should eql(@user)
+      end
+    end
+
+    context "when the email exists" do
+      before do
+        @email = create(:email, address: 'bob@bob.com')
+      end
+
+      it "should not create a new user" do
+        expect {
+          User.from_email('bob@bob.com')
+        }.to_not change(User, :count)
+      end
+
+      it "should not create a new email" do
+        expect {
+          User.from_email('bob@bob.com')
+        }.to_not change(Email, :count)
+      end
+
+      it "should return the existing user" do
+        found_user = User.from_email('bob@bob.com')
+
+        found_user.should eql(@email.user)
+      end
+    end
+  end
 end
