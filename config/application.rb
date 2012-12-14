c93831fde937f8967e33d4e7bfdffd5a8d0590cb
Randy Schmidt <randy@forge38.com>
2012-12-14 12:28:15 -0500

    Don't initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -71,6 +71,8 @@ module Diddleberry
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     # config.assets.precompile += %w( admin.js admin.css )
   end
 end
