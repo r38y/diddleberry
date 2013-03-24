f151488c3ac7b76ca9b8b0e8be17225395e98fe7
Randy Schmidt <me@r38y.com>
2013-03-24 18:38:45 -0400

    Heroku config example

---
+++ config/heroku_env_example.rb
@@ -0,0 +1,7 @@
+# copy this file to config/heroku_env.rb and edit
+# This file contains the ENV vars necessary to run the app locally.
+# Some of these values are sensitive, and some are developer specific.
+#
+# DO NOT CHECK THIS FILE INTO VERSION CONTROL
+
+ENV['POSTMARK_API_KEY'] = 'CHANGEME'
